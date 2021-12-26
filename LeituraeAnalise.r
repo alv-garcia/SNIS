073aa026a988b387309f28ec55788b8e21fbb297
alv-garcia <alvaro.emannuel42@gmail.com>
2021-12-26 12:41:24 -0300

    Update 001

--- LeituraeAnalise.r
+++ LeituraeAnalise.r
@@ -9,3 +9,44 @@ library(tidyverse)
 
 
 #---------- Função de leitura e limpeza--------------
+
+#Definir o diretório de trabalho onde estão os arquivos
+setwd("D:/DEV/SNIS")
+
+# Definir o nome do arquivo
+Arquivo <- "Agregado Sudeste.csv"
+
+
+# Função para leitura e limpeza do arquivo - o segundo argumento define se o data frame solicitado deve
+# vir em formato "Tidy" (Adicionar o TRUE)
+LeituraSnis = function(Arquivo, Tidy ){
+
+  db<-read.csv2(Arquivo,header = TRUE, skipNul = TRUE, row.names = NULL)
+
+  nomes<-db %>%
+    colnames()
+
+  iniciais <-c("CODIGO","Municipio","Estado","Ano","COD_PREST", "Prestador","SIGLA","Abrangencia","Servico","Natureza")
+  indicadores <- str_sub(nomes[11:length(nomes)], start = 1, end = 6)
+  indicadores <-str_remove_all(indicadores,"[_.]")
+
+  colnames(db)<-c(iniciais,indicadores)
+
+  if( Tidy == "TRUE"){
+
+  td<-gather(db,colnames(db[11]):colnames(db[length(db)]), key = indicador, value = valores)
+
+  return(td)
+
+  } else{
+
+  return(db)
+
+  }
+}
+
+
+# Exemplo
+dados <- LeituraSnis(Arquivo, FALSE)
+
+glimpse(dados)
