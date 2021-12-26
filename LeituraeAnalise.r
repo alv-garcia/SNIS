7c69c09e26e0379d065c2a8c95c39bf6444ab4bd
alv-garcia <alvaro.emannuel42@gmail.com>
2021-12-26 12:20:47 -0300

    Criação do Arquivo principal

---
+++ LeituraeAnalise.r
@@ -0,0 +1,11 @@
+## Leitura e análise de dados do SNIS
+
+# Criação: 26/12/2021
+
+
+#---------- Pacotes -------------
+
+library(tidyverse)
+
+
+#---------- Função de leitura e limpeza--------------
